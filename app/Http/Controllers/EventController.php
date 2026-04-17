a3591ed9b088ee38e5f40ba0dae74bf9e2ce9b24
rafliabid <rafliabid@students.amikom.ac.id>
2026-04-17 14:53:12 +0700

    tugas 2

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,21 +6,13 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    // Merender halaman detail event
+    //
     public function show()
     {
         return view('event-detail');
     }
 
-    // Merender halaman checkout
-    public function checkout()
-    {
+    public function checkout(){
         return view('checkout');
-    }
-
-    // Merender halaman ticket 
-    public function ticket()
-    {
-        return view('ticket');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,43 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-// Rute Home
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/event/1', [EventController::class, 'show'])->name('events.show');
+Route::get('/checkout', [EventController::class, 'checkout'])->name('checkout');
+Route::get('/my-ticket', [EventController::class, 'ticket'])->name('ticket');
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
 
-// Rute Kontak 
-Route::get('/kontak', function () {
-    return view('contact');
-});
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-// LATIHAN: Rute Profil
-Route::get('/profil', function () {
-    return view('profil');
+Route::get('/events', [EventController::class, 'show']);
+// dan seterusnya...
 });
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-// LATIHAN: Rute Katalog (AmikomEventHub)
-Route::get('/katalog', function () {
-    return view('katalog');
-});
+// Route::get('/tentang', function() {
+//     return '<h1>Ini adalah halaman tentang aplikasi Event Hub</h1>';
+// });
 
-// LATIHAN: Rute Bantuan (FAQ)
-Route::get('/bantuan', function () {
-    return view('bantuan');
-});
+// Route::get('/kontak', function() {
+//     return view('contact');
+// });
+
+// Route::get('/profile', function(){
+//     return view('profile');
+// });
+
+// Route::get('/katalog', function(){
+//     return view('katalog');
+// });
+
+// Route::get('/bantuan', function(){
+//     return view('bantuan');
+// });
+
+// Route::get('/',)
