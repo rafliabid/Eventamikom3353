5e06799a2ba74527a02fefb41b746d871af1add0
rafliabid <rafliabid@students.amikom.ac.id>
2026-05-08 11:01:10 +0700

    tugas 4

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,23 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\EventController as EventAdminController;
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::resource('events', EventAdminController::class);
+});
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/event/1', [EventController::class, 'show'])->name('events.show');
 Route::get('/checkout', [EventController::class, 'checkout'])->name('checkout');
 Route::get('/my-ticket', [EventController::class, 'ticket'])->name('ticket');
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+// Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
 
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+// Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('/events', [EventController::class, 'show']);
+// Route::get('/events', [EventController::class, 'show']);
 // dan seterusnya...
-});
+// });
 // Route::get('/', function () {
 //     return view('welcome');
 // });
